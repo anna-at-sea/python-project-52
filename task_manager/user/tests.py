f33f7b29819e0d678e2a95b88ce6ca860e74476a
Anna Smertina <smr.anya@gmail.com>
2024-12-18 20:55:46 -0600

    Organize imports (8)

--- task_manager/user/tests.py
+++ task_manager/user/tests.py
@@ -1,10 +1,10 @@
+import json
 from os.path import join
 
 from django.contrib.messages import get_messages
 from django.test import TestCase
 from django.urls import reverse
 from django.utils.translation import gettext as _
-import json
 
 from .models import User
 
