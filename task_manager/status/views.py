df95efa8af9720e89b84a568d70acffd13f4cef8
Anna Smertina <smr.anya@gmail.com>
2024-12-24 00:25:56 -0600

    Fix imports order in views (3)

--- task_manager/status/views.py
+++ task_manager/status/views.py
@@ -4,8 +4,9 @@ from django.views.generic.list import ListView
 
 from task_manager.status.forms import StatusForm
 from task_manager.status.models import Status
-from task_manager.utils import CreateViewMixin, DeleteViewMixin, \
-    UpdateViewMixin, UserLoginRequiredMixin
+from task_manager.utils import (
+    CreateViewMixin, DeleteViewMixin, UpdateViewMixin, UserLoginRequiredMixin
+)
 
 
 class StatusIndexView(UserLoginRequiredMixin, ListView):
